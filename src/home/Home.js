9cbe6120c16e1d6f52266b50e208c760933fc3a9
Hayley Smith <hayley.j.smith@gmail.com>
2020-08-07 13:30:54 -0500

    added a few things to home page. login still not quite working

---
+++ src/home/Home.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div className="home-container">
+                <div className="container">
+                    <div className="graf-bg-container">
+                        <div className="graf-layout">
+                            <div className="graf-circle">Welcome to Equester</div>
+                            <div className="graf-circle">For Equestrians with Questions</div>
+                            <div className="graf-circle">Welcome to Equester</div>
+                            <div className="graf-circle">For Equestrians with Questions</div>
+                            <div className="graf-circle">Welcome to Equester</div>
+                            <div className="graf-circle">For Equestrians with Questions</div>
+                            <div className="graf-circle">Welcome to Equester</div>
+                            <div className="graf-circle">For Equestrians with Questions</div>
+                            <div className="graf-circle">Welcome to Equester</div>
+                            <div className="graf-circle">For Equestrians with Questions</div>
+                            <div className="graf-circle">Welcome to Equester</div>
+                        </div>
+                    </div>
+                    <h1 className="home-title">For Equestrians with Questions</h1>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Home;
